c04951bd6a3cf4a258bca60a5f12c863046dbdb0
Michal Szaj <software@michalszaj.eu>
2021-01-13 10:52:50 +0100

    initial change of left dice image

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceWindowLeft: UIImageView!
+    
+    @IBOutlet weak var diceWindowRight: UIImageView!
+    
+    @IBOutlet weak var rollButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceWindowLeft.image = #imageLiteral(resourceName: "DiceSix")
     }
 
 
